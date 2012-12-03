2273933e725d18ca26c40f1e1342ca4cb523947f
matan <matan@cloudaloe.org>
2012-12-03 14:38:25 +0200

    no comment

--- mySample.js
+++ mySample.js
@@ -51,7 +51,7 @@ var extensions = [new openid.UserInterface(),
                       })];
 
 var relyingParty = new openid.RelyingParty(
-    'http://example.com/verify', // Verification URL (yours)
+    'http://warm-anchorage-1114.herokuapp.com/', // Verification URL (yours)
     null, // Realm (optional, specifies realm for OpenID authentication)
     false, // Use stateless verification
     false, // Strict mode
